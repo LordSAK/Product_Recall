124e6848d765e833fe22c7d3770e60dfcc9515d8
Saghir A. Khatri <saghir.alam@musewerx.com>
2013-07-05 18:21:41 +0500

    changeprod1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,5 @@ ProductRecall::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  config.action_controller.consider_all_requests_local = true
 config.action_controller.perform_caching             = false
 end
