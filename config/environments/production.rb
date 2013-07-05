b5d3edb0be0892b34041a8300d24904fe7614709
Saghir A. Khatri <saghir.alam@musewerx.com>
2013-07-05 18:18:15 +0500

    changeprod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,5 @@ ProductRecall::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   config.action_controller.consider_all_requests_local = true
-config.action_view.debug_rjs                         = true
 config.action_controller.perform_caching             = false
 end
