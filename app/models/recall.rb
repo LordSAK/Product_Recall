b6eb486131d1c13e109678e074d7e5b09dc051d8
Saghir A. Khatri <saghir.alam@musewerx.com>
2013-07-05 20:44:38 +0500

    Finish change model of recall query1

--- app/models/recall.rb
+++ app/models/recall.rb
@@ -3,7 +3,7 @@ class Recall < ActiveRecord::Base
 
 	def self.search(search)
 		if search
-    		find(:all, :conditions => ['"LOWER(Category)" LIKE ? or "LOWER(Details)" LIKE ? or "LOWER(Summary)" LIKE ? or "LOWER(Title)" LIKE ? or "LOWER(Manufacturer)" LIKE ? or "LOWER(Products)" LIKE ? or "LOWER(Hazards)" LIKE ?', "%#{search.downcase}%", "%#{search.downcase}%", "%#{search.downcase}%", "%#{search.downcase}%", "%#{search.downcase}%", "%#{search.downcase}%", "%#{search.downcase}%"])
+    		find(:all, :conditions => ['"Category" LIKE ? or "Details" LIKE ? or "Summary" LIKE ? or "Title" LIKE ? or "Manufacturer" LIKE ? or "Products" LIKE ? or "Hazards" LIKE ?', "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%"])
   		else	
     		find(:all)
   		end
