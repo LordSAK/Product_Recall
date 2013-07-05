495a5bb1d874e57e9b6d77df05c586f72b1b6955
Saghir A. Khatri <saghir.alam@musewerx.com>
2013-07-05 18:05:59 +0500

    change heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,8 @@ ProductRecall::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  config.action_controller.consider_all_requests_local = true
+config.action_view.debug_rjs                         = true
+config.action_controller.perform_caching             = false
 end
