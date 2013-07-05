4537f70a16510202c725a42c8b9acca88c0a4776
Saghir A. Khatri <saghir.alam@musewerx.com>
2013-07-05 19:53:28 +0500

    finish updatequery prod haz

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,8 +2,8 @@ class SearchController < ApplicationController
   
   def Search
   	@recalls = Recall.search(params[:Searching])
-  	@products = Recall.count(:group => :Products)
-  	@hazards = Recall.count(:group => :Hazards)
+  	@products = Recall.count(:group => '"Products"')
+  	@hazards = Recall.count(:group => '"Hazards"')
   	if !params[:from].nil? and !params[:to].nil?
   		@from=Date.strptime params[:from],'%m/%d/%Y'
   		@to=Date.strptime params[:to],'%m/%d/%Y'
