e21bae5ac6e83cb530631c31c49019cfb6ca3d7f
Saghir A. Khatri <saghir.alam@musewerx.com>
2014-01-02 16:16:05 +0500

    calendar change 4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ ProductRecall::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -65,5 +65,5 @@ ProductRecall::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-config.action_controller.perform_caching             = false
+  config.action_controller.perform_caching  = false
 end
