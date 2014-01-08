9f6587405f43a39e41f4d6336cc7a66e14b13611
Saghir A. Khatri <saghir.alam@musewerx.com>
2014-01-08 18:17:49 +0500

    history3

--- app/models/history.rb
+++ app/models/history.rb
@@ -2,6 +2,6 @@ class History < ActiveRecord::Base
   set_primary_key :id 
   belongs_to :user
   attr_accessible :TimeLogin, :TimeLogout, :user_id
-  default_scope -> {order('created_at DESC')}
+  default_scope -> {order('Date("created_at") DESC')}
   validates :user_id, presence: true
 end
